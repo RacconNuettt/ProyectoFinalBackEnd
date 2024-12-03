0ed8c9d56baa0714b0b78b5df5709cc8513571f0
Darielforward <dcerdas@fwdcostarica.com>
2024-12-03 11:00:51 -0600

    register e inicio de sesion tuneados

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,58 +1,32 @@
 import React, { useState } from 'react';
-import { TextField, Button, Typography, Container, Box, Divider } from '@mui/material';
+import {
+    TextField,
+    Button,
+    Typography,
+    Container,
+    Box,
+    Divider,
+    useMediaQuery,
+} from '@mui/material';
 import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { styled } from '@mui/system';
-import { registerClient } from '../services/client';  
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import plantaloginyregister from '../assets/plantaloginyregister.png'; // Import the image
+import { registerClient } from '../services/client';
 
-const CustomContainer = styled(Container)({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    fontFamily: "'Patrick Hand', cursive",
-});
-
-const FormContainer = styled(Box)({
-    width: '30%',
-    backgroundColor: '#008000bb',
-    textAlign: 'center',
-    color: 'white',
-    padding: '2rem',
-    borderRadius: '8px',
-    backgroundImage: 'url(src/assets/plantaloginyregister.png)',  // Corregido el error de la ruta
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: '100px bottom',
-});
-
-const StyledTextField = styled(TextField)({
-    marginBottom: '1rem',
-    backgroundColor: 'white',
-    borderRadius: '5px',
-    '& label': {
-        color: 'grey',
-        fontFamily: "'Patrick Hand', cursive",
-    },
-    '& .MuiOutlinedInput-root': {
-        '& input': {
-            fontFamily: "'Patrick Hand', cursive",
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#28a745', // Button green
         },
-        '& fieldset': {
-            borderColor: 'white',
-        }
+        background: {
+            default: '#f8f9fa', // General background
+            paper: '#008000bb', // Form background
+        },
     },
-});
-
-const CustomButton = styled(Button)({
-    backgroundColor: '#fff',
-    color: '#28a745',
-    fontFamily: "'Patrick Hand', cursive",
-    border: '1px solid #28a745',
-    marginTop: '1rem',
-    '&:hover': {
-        backgroundColor: '#28a745',
-        color: '#fff',
+    typography: {
+        fontFamily: "'Patrick Hand', cursive",
     },
 });
 
@@ -60,26 +34,32 @@ const RegisterForm = () => {
     const [formData, setFormData] = useState({
         clientname: '',
         clientemail: '',
-        clientpassword: ''
+        clientpassword: '',
     });
 
-    const navigate = useNavigate(); 
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    // Media query to detect small screens
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.id]: e.target.value });
     };
 
-    const validateForm = async () => {
+    const validateForm = () => {
         const { clientname, clientemail, clientpassword } = formData;
 
-        if (!clientname || !clientemail || !clientpassword) { 
+        if (!clientname || !clientemail || !clientpassword) {
             toast.error("Todos los campos son obligatorios");
             return false;
         }
+
         if (clientpassword.length < 8) {
             toast.error("La contraseña debe tener al menos 8 caracteres");
             return false;
         }
+
         const hasUpperCase = /[A-Z]/.test(clientpassword);
         const hasLowerCase = /[a-z]/.test(clientpassword);
         const hasNumber = /\d/.test(clientpassword);
@@ -95,65 +75,160 @@ const RegisterForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (await validateForm()) {
+        if (validateForm()) {
+            setLoading(true);
             try {
-                const response = await registerClient(formData);
+                await registerClient(formData);
                 toast.success("Usuario registrado exitosamente");
                 setFormData({
                     clientname: '',
                     clientemail: '',
-                    clientpassword: ''
+                    clientpassword: '',
                 });
-                setTimeout(() => {
-                    navigate('/login');
-                }, 2000); 
+                setTimeout(() => navigate('/login'), 2000);
             } catch (error) {
                 toast.error("Hubo un error al registrar el usuario");
+            } finally {
+                setLoading(false);
             }
         }
     };
 
     return (
-        <CustomContainer>
-            <FormContainer>
-                <Typography variant="h4" gutterBottom sx={{fontFamily:"'Patrick Hand', cursive"}}>Regístrate</Typography>
-                <Divider sx={{ backgroundColor: 'white', margin: 'rem 0' }} />
-                <form onSubmit={handleSubmit}>
-                    <StyledTextField
-                        label="Nombre Completo"
-                        placeholder="Nombre"
-                        id="clientname"  
-                        value={formData.clientname}
-                        onChange={handleChange}
-                        fullWidth
-                    />
-                    <StyledTextField
-                        label="Correo Electrónico"
-                        placeholder="Email"
-                        id="clientemail"  // Usamos 'clientemail' como en el estado
-                        value={formData.clientemail}
-                        onChange={handleChange}
-                        fullWidth
-                    />
-                    <StyledTextField
-                        label="Contraseña"
-                        placeholder="Contraseña"
-                        id="clientpassword"  // Usamos 'clientpassword' como en el estado
-                        type="password"
-                        value={formData.clientpassword}
-                        onChange={handleChange}
-                        fullWidth
-                    />
-                    <CustomButton type="submit" fullWidth>
-                        Registrar
-                    </CustomButton>
-                </form>
-                <Typography variant="body2" mt={2} sx={{fontFamily:"'Patrick Hand', cursive"}}>
-                    Ya tienes una cuenta? <Link to="/Login" style={{ color: 'white', textDecoration: 'underline' }}>Inicia Sesión</Link>
-                </Typography>
-            </FormContainer>
-            <ToastContainer />
-        </CustomContainer>
+        <ThemeProvider theme={theme}>
+            <Container
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                    backgroundColor: 'background.default',
+                    px: 2,
+                }}
+            >
+                <Box
+                    sx={{
+                        width: { xs: '100%', sm: '80%', md: '50%', lg: '30%' },
+                        bgcolor: 'background.paper',
+                        textAlign: 'center',
+                        color: 'text.primary',
+                        p: { xs: 2, sm: 3, md: 4 },
+                        borderRadius: 2,
+                        backgroundImage: `url(${plantaloginyregister})`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center bottom',
+                    }}
+                >
+                    <Typography
+                        variant="h4"
+                        gutterBottom
+                        sx={{
+                            color: '#000000',
+                            fontSize: { xs: '1.8rem', md: '2.2rem' }, 
+                            color:'#ffffff',
+                        }}
+                    >
+                        Regístrate
+                    </Typography>
+                    <Divider sx={{ backgroundColor: 'text.primary', mb: 2 }} />
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            //label="Nombre Completo"
+                            placeholder="Nombre"
+                            id="clientname"
+                            value={formData.clientname}
+                            onChange={handleChange}
+                            fullWidth
+                            sx={{
+                                color: '#ffffff',
+                                mb: 2,
+                                backgroundColor: '#fff',
+                                borderRadius: 1,
+                                '& label': { color: 'text.secondary' },
+                                '& input': { fontFamily: 'Patrick Hand, cursive' },
+                                '& fieldset': { borderColor: 'white' },
+                                '&:hover fieldset': { borderColor: 'primary.main' },
+                            }}
+                        />
+                        <TextField
+                            //label="Correo Electrónico"
+                            placeholder="Email"
+                            id="clientemail"
+                            value={formData.clientemail}
+                            onChange={handleChange}
+                            fullWidth
+                            sx={{
+                                mb: 2,
+                                backgroundColor: '#fff',
+                                borderRadius: 1,
+                                '& label': { color: 'text.secondary' },
+                                '& input': { fontFamily: 'Patrick Hand, cursive' },
+                                '& fieldset': { borderColor: 'white' },
+                                '&:hover fieldset': { borderColor: 'primary.main' },
+                            }}
+                        />
+                        <TextField
+                            //label="Contraseña"
+                            placeholder="Contraseña"
+                            id="clientpassword"
+                            type="password"
+                            value={formData.clientpassword}
+                            onChange={handleChange}
+                            fullWidth
+                            sx={{
+                                mb: 2,
+                                backgroundColor: '#fff',
+                                borderRadius: 1,
+                                '& label': { color: 'text.secondary' },
+                                '& input': { fontFamily: 'Patrick Hand, cursive' },
+                                '& fieldset': { borderColor: 'white' },
+                                '&:hover fieldset': { borderColor: 'primary.main' },
+                            }}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            sx={{
+                                backgroundColor: '#fff',
+                                color: 'primary.main',
+                                fontFamily: "'Patrick Hand', cursive",
+                                border: '1px solid',
+                                borderColor: 'primary.main',
+                                mt: 2,
+                                '&:hover': {
+                                    backgroundColor: 'primary.main',
+                                    color: '#fff',
+                                },
+                            }}
+                            disabled={loading}
+                        >
+                            {loading ? 'Registrando...' : 'Registrar'}
+                        </Button>
+                    </form>
+                    <Typography
+                        variant="body2"
+                        mt={2}
+                        sx={{
+                            mt: 2,
+                            fontSize: { xs: '0.8rem', sm: '1rem' },
+                            color:'#ffffff',
+                        }}
+                    >
+                        Ya tienes una cuenta?{" "}
+                        <Link
+                            to="/login"
+                            style={{
+                                color: 'white',
+                                textDecoration: 'underline',
+                            }}
+                        >
+                            Inicia Sesión
+                        </Link>
+                    </Typography>
+                </Box>
+                <ToastContainer />
+            </Container>
+        </ThemeProvider>
     );
 };
 
